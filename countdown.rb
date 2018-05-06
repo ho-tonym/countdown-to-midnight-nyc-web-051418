c3111b026d403e848196927a0f55ba4b9777eff0
Tony Ho <ho.tonym@gmail.com>
2018-05-06 19:55:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,5 +6,16 @@ def countdown(integer)
     "#{integer} SECOND(S)!"
   counter -=1
   end
-  "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+
+  counter = integer
+    while counter > 0 do
+      "#{integer} SECOND(S)!"
+      sleep(1)
+    counter -=1
+    end
+  puts "HAPPY NEW YEAR!"
 end
