8602383dec8592866588efbb35e3f87b2f6017f4
Tony Ho <ho.tonym@gmail.com>
2018-05-06 19:53:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  counter = integer
+  while counter > 0 do
+    "#{integer} SECOND(S)!"
+  counter -=1
+  end
+  "HAPPY NEW YEAR!"
 end
